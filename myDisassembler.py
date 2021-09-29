fa1937c725e66ba965fd39700bc754465cf269b6
Nate Doggett <nate88@vt.edu>
2021-09-29 13:27:39 -0400

    Initial commit

---
+++ myDisassembler.py
@@ -0,0 +1,8 @@
+# Project 1 - Disassembler
+# ECE 3504, Fall 2021
+# Nate Doggett
+
+# Description: This program disassembles MIPS machines code and coverts
+# the content into human-readable MIPS assembly
+
+file = open('test.txt', 'r')
