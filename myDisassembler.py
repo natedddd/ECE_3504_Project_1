102b7ee88513d75ed66619c9dbe34b5ca0866774
Nate Doggett <nate88@vt.edu>
2021-10-05 13:24:07 -0400

    Add functionality for I & R type instruction. Missing branch implementation

--- myDisassembler.py
+++ myDisassembler.py
@@ -6,10 +6,40 @@ Nate Doggett
 Description: This program disassembles MIPS machines code and coverts
 the content into human-readable MIPS assembly
 """
+class RType:
+    """ RType instruction type """
+    def __init__(self):
+        self.op = None
+        self.rs = None
+        self.rt = None
+        self.rd = None
+        self.shamt = None
+        self.funct = None
 
+    def parseRType(self, line):
+        self.op = "000000"
+        self.rs = line[6:11]
+        self.rt = line[11:16]
+        self.rd = line[16:21]
+        self.shamt = line[21:26]
+        self.funct = line[26:32]
+
+class IType:
+    """ IType instruction type """
+    def __init__(self):
+        self.op = None
+        self.rs = None
+        self.rt = None
+        self.immed = None
+
+    def parseIType(self, line):
+        self.op = line[0:6]
+        self.rs = line[6:11]
+        self.rt = line[11:16]
+        self.immed = line[16:32]
 
 # dictionary of are R_Type and I_Type binary equivalents
-r_type = {  '100000':'add',
+r_type_op_codes = {  '100000':'add',
             '100001':'addu',
             '100100':'and', # may be 0x24
             '100111':'nor',
@@ -20,7 +50,7 @@ r_type = {  '100000':'add',
             '000010':'srl',
             '100010':'sub',
             '100011':'subu'}
-i_type = {  '001000':'addi',
+i_type_op_codes = {  '001000':'addi',
             '001001':'addiu',
             '001100':'addi',
             '000100':'beq',
@@ -29,6 +59,7 @@ i_type = {  '001000':'addi',
             '100101':'lhu',
             '100100':'ll',
             '001111':'lui',
+            '100011':'lw',
             '001101':'ori',
             '001010':'slti',            
             '001011':'sltiu',
@@ -36,41 +67,104 @@ i_type = {  '001000':'addi',
             '111000':'sc',
             '101001':'sh', 
             '101011':'sw'}
+register_dict = {   '00000':'$zero',
+                    '00001':'$at',
+                    '00010':'$v0',
+                    '00011':'$v1',
+                    '00100':'$a0',
+                    '00101':'$a1',
+                    '00110':'$a2',
+                    '00111':'$a3',
+                    '01000':'$t0',
+                    '01001':'$t1',
+                    '01010':'$t2',
+                    '01011':'$t3',
+                    '01100':'$t4',
+                    '01101':'$t5',
+                    '01110':'$t6',
+                    '01111':'$t7',
+                    '10000':'$s0',
+                    '10001':'$s1',
+                    '10010':'$s2',
+                    '10011':'$s3',
+                    '10100':'$s4',
+                    '10101':'$s5',
+                    '10110':'$s6',
+                    '10111':'$s7',
+                    '11000':'$t8',
+                    '11001':'$t9',
+                    '11010':'$k0',
+                    '11011':'$k1',
+                    '11100':'$gp',
+                    '11101':'$sp',
+                    '11110':'$fp',
+                    '11111':'$ra'}
+
 
 file = open('test.txt', 'r')
 f = file.readlines()
 file.close()
 
 binaryEquiv = []
+tempOutputList = {}
+labelList = {}
+programCounter = 0
+
 for line in f:
     str1 = line.strip()
     res = "{0:08b}".format(int(str1, 16)) # converts the hex string into binary
     resFilled = res.zfill(32) # fills the result with any leading zeros
     binaryEquiv.append(resFilled)
 
-for val in binaryEquiv:
-    print(val)
+# for val in binaryEquiv:
+#     print(val)
 
 
-def parseRType(line):
-    op = line[0:6]
-    rs = line[6:11]
-    rt = line[11:16]
-    rd = line[16:21]
-    shamt = line[21:26]
-    funct = line[26:32]
-
-
-def parseIType(line):
-    op = line[0:6]
-    rs = line[6:11]
-    rt = line[11:16]
-    immed = line[16:32]
+my_rtype = RType()
+my_itype = IType()
+isRType = False
 
 for line in binaryEquiv:
-    if line[0:5] == '0000':
-        parseRType(line)
+    if (line[0:6] == '000000'):
+        my_rtype.parseRType(line)
+        isRType = True
     else:
-        parseIType(line)
+        my_itype.parseIType(line)
+        isRType = False
 
+    if isRType:
+        op_str = r_type_op_codes[my_rtype.funct]
+        rs_str = register_dict[my_rtype.rs]
+        rt_str = register_dict[my_rtype.rt]
+        rd_str = register_dict[my_rtype.rd]
+        shamt_str = int(my_rtype.shamt,2)
+        
+        temp_int = int(my_rtype.funct,2)
+        funct_str = hex(temp_int)
+        # print(shamt_str)
+        # print()
+        if (shamt_str == 0):
+            tempOutputList[programCounter] = op_str + " " + rd_str + ", " + rs_str + ", " + rt_str 
+        else:
+            tempOutputList[programCounter] = op_str + " " + rd_str + ", " + rt_str + ", " + str(shamt_str)
 
+    # IType case
+    else: 
+        op_str = i_type_op_codes[my_itype.op]
+        rs_str = register_dict[my_itype.rs]
+        rt_str = register_dict[my_itype.rt]
+        immed_str = int(my_itype.immed,2)
+        if (immed_str > 60000):
+            immed_str = immed_str-65536
+        #immed_str = int.from_bytes(my_itype.immed, byteorder='big', signed=True)
+
+        if (op_str == "lw" or op_str == "sw"):
+            tempOutputList[programCounter] = op_str + " " + rt_str + ", " + str(immed_str) + "(" + rs_str + ")"  
+        else:
+            tempOutputList[programCounter] = op_str + " " + rt_str + ", " + rs_str + ", " + str(immed_str) 
+
+    programCounter += 1
+
+for i in tempOutputList:
+    print(tempOutputList[i])
+        # print(tempOutputList[programCounter])
