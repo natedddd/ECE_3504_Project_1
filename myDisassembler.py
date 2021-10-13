47583f4a4009cc97101c51361e4cd172ea2bb5f7
Nate Doggett <nate88@vt.edu>
2021-10-13 09:13:46 -0400

    Add improved error handling. Finished.

--- myDisassembler.py
+++ myDisassembler.py
@@ -10,6 +10,13 @@ import sys
 class RType:
     """ RType instruction type """
     def __init__(self):
+        """
+        This class is built to handle an R-Type MIPS
+        assembly language instruction. The R-Type is
+        in the form of an opcode (ALL ZEROS),
+        rs, rt, and rd registers, a shift amount,
+        and a function encoding
+        """
         self.op = None
         self.rs = None
         self.rt = None
@@ -18,6 +25,14 @@ class RType:
         self.funct = None
 
     def parseRType(self, line):
+        """ 
+        Parses a 32-bit line into the proper
+        opcode, registers, and immediate value expected
+        from an R-Type instruction
+
+        @param: line    A 32-bit string containing the MIPS
+                        instruction in binary
+        """
         self.op = "000000"
         self.rs = line[6:11]
         self.rt = line[11:16]
@@ -28,48 +43,64 @@ class RType:
 class IType:
     """ IType instruction type """
     def __init__(self):
+        """
+        This class is built to handle an I-Type MIPS
+        assembly language instruction. The I-Type is
+        in the form of an opcode, rs and rt regsiter,
+        and an immediate value 
+        """
         self.op = None
         self.rs = None
         self.rt = None
         self.immed = None
 
     def parseIType(self, line):
+        """ 
+        Parses a 32-bit line into the proper
+        opcode, registers, and immediate value expected
+        from an I-Type instruction
+
+        @param: line    A 32-bit string containing the MIPS
+                        instruction in binary
+        """
         self.op = line[0:6]
         self.rs = line[6:11]
         self.rt = line[11:16]
         self.immed = line[16:32]
 
+BYTE_LENGTH = 4
+
 def main(inputStr):
     hasError = False
     # dictionary of are R_Type and I_Type binary equivalents
-    r_type_op_codes = {  '100000':'add',
-                '100001':'addu',
-                '100100':'and', # may be 0x24
-                '100111':'nor',
-                '100101':'or',
-                '101010':'slt',
-                '101011':'sltu',
-                '000000':'sll',
-                '000010':'srl',
-                '100010':'sub',
-                '100011':'subu'}
-    i_type_op_codes = {  '001000':'addi',
-                '001001':'addiu',
-                '001100':'addi',
-                '000100':'beq',
-                '000101':'bne',
-                '100100':'lbu',
-                '100101':'lhu',
-                '100100':'ll',
-                '001111':'lui',
-                '100011':'lw',
-                '001101':'ori',
-                '001010':'slti',            
-                '001011':'sltiu',
-                '101000':'sb',
-                '111000':'sc',
-                '101001':'sh', 
-                '101011':'sw'}
+    r_type_op_codes = { '100000':'add',
+                        '100001':'addu',
+                        '100100':'and',
+                        '100111':'nor',
+                        '100101':'or',
+                        '101010':'slt',
+                        '101011':'sltu',
+                        '000000':'sll',
+                        '000010':'srl',
+                        '100010':'sub',
+                        '100011':'subu'}
+    i_type_op_codes = { '001000':'addi',
+                        '001001':'addiu',
+                        '001100':'addi',
+                        '000100':'beq',
+                        '000101':'bne',
+                        '100100':'lbu',
+                        '100101':'lhu',
+                        '100100':'ll',
+                        '001111':'lui',
+                        '100011':'lw',
+                        '001101':'ori',
+                        '001010':'slti',            
+                        '001011':'sltiu',
+                        '101000':'sb',
+                        '111000':'sc',
+                        '101001':'sh', 
+                        '101011':'sw'}
     register_dict = {   '00000':'$zero',
                         '00001':'$at',
                         '00010':'$v0',
@@ -111,54 +142,35 @@ def main(inputStr):
     f = file.readlines()
     file.close()
 
-    binaryEquiv = []
     tempOutputList = {}
     addressesThatNeedLabels = []
     programCounter = 0
     lineCounter = 1
 
     for line in f:
-        str1 = line.strip()
-        if (len(str1) != 8):
-            print("Cannot dissassemble " + str1 + " at line " + str(lineCounter) + " due to invalid length.")
-            hasError = True
-        else:
-            try:
-                res = "{0:08b}".format(int(str1, 16)) # converts the hex string into binary
-                resFilled = res.zfill(32) # fills the result with any leading zeros
-                binaryEquiv.append(resFilled)
-            except:
-                hasError = True
-                print("Cannot dissassemble " + str1 + " at line " + str(lineCounter) + " due to invalid length.")
-        lineCounter += 1
+        try:
+            str1 = line.strip()
 
-    my_rtype = RType()
-    my_itype = IType()
-    isRType = False
-    lineCounter = 1
+            res = "{0:08b}".format(int(str1, 16)) # converts the hex string into binary
+            resFilled = res.zfill(32) # fills the result with any leading zeros
 
-    for line in binaryEquiv:
-        if (line[0:6] == '000000'):
-            my_rtype.parseRType(line)
-            isRType = True
-        else:
-            my_itype.parseIType(line)
+            my_rtype = RType() # reused objects for each pass
+            my_itype = IType() # reused objects for each pass
             isRType = False
 
-        if isRType:
-            if  (r_type_op_codes.get(my_rtype.funct) is None) or (register_dict.get(my_rtype.rs) is None) or (register_dict.get(my_rtype.rt) is None) or (register_dict.get(my_rtype.rd) is None):
-                str2 = (f[lineCounter-1]).strip()
-                print("Cannot dissassemble " + str2 + " at line " + str(lineCounter) + " due to invalid input.")
-                hasError = True
+            if (line[0:6] == '000000'):
+                my_rtype.parseRType(resFilled)
+                isRType = True
             else:
+                my_itype.parseIType(resFilled)
+                isRType = False
+
+            if isRType:
                 op_str = r_type_op_codes[my_rtype.funct]
                 rs_str = register_dict[my_rtype.rs]
                 rt_str = register_dict[my_rtype.rt]
                 rd_str = register_dict[my_rtype.rd]
                 shamt_str = int(my_rtype.shamt,2)
-                
-                temp_int = int(my_rtype.funct,2)
-                funct_str = hex(temp_int)
 
                 # only has a different format if a shift amount is given
                 if (shamt_str == 0):
@@ -166,20 +178,15 @@ def main(inputStr):
                 else:
                     tempOutputList[programCounter] = "\t" + op_str + " " + rd_str + ", " + rt_str + ", " + str(shamt_str) + "\n"
 
-        # IType case
-        else: 
-            if  (i_type_op_codes.get(my_itype.op) is None) or (register_dict.get(my_itype.rs) is None) or (register_dict.get(my_itype.rt) is None):
-                str2 = (f[lineCounter-1]).strip()
-                print("Cannot dissassemble " + str2 + " at line " + str(lineCounter) + " due to invalid input.")
-                hasError = True
-            else:
+            # IType case
+            else: 
                 op_str = i_type_op_codes[my_itype.op]
                 rs_str = register_dict[my_itype.rs]
                 rt_str = register_dict[my_itype.rt]
                 immed_str = int(my_itype.immed,2)
 
                 # if the immed_str should be a negative signed value
-                if (immed_str > 60000):
+                if (immed_str > 50000):
                     immed_str = immed_str-65536
 
                 # format for lw and sw
@@ -188,7 +195,7 @@ def main(inputStr):
 
                 # format for beq or bne
                 elif (op_str == "beq" or op_str == "bne"):
-                    address = 4*(programCounter + immed_str + 1)
+                    address = BYTE_LENGTH*(programCounter + immed_str + 1)
                     hexAddress = str( hex(address) )
                     hexAddress = hexAddress[2:]
                     addressStr = "Addr_" +  hexAddress.zfill(4)
@@ -198,26 +205,34 @@ def main(inputStr):
                 # all other IType formats
                 else:
                     tempOutputList[programCounter] = "\t" + op_str + " " + rt_str + ", " + rs_str + ", " + str(immed_str) + "\n"
-        programCounter += 1
-        lineCounter += 1
+            programCounter += 1
+            lineCounter += 1
 
+        # catches and handles any errors thrown while attmepting to dissassemble
+        except:
+            output = f[lineCounter-1].strip()
+            print(f"Cannot dissasemble {output} at line {lineCounter}")
+            hasError = True
+            programCounter += 1
+            lineCounter += 1
+
+    # if there where no errors, write to <file_name>.s
     if not hasError:
-        fileOut = open(file.name[:-4] + ".s", "w")
+        fileOut = open(file.name[:-4] + ".s", "w") # let the final name be name minus .obj
 
-        for i in range (0,programCounter):
-            if i in addressesThatNeedLabels:
-                address = 4*i
+        for idx in range (0,programCounter):
+            if idx in addressesThatNeedLabels:
+                address = BYTE_LENGTH*idx
                 hexAddress = str( hex(address) )
                 hexAddress = hexAddress[2:]
                 addressStr = "Addr_" +  hexAddress.zfill(4) + ":\n"
                 fileOut.write(addressStr)
 
-            fileOut.write(tempOutputList[i])
+            fileOut.write(tempOutputList[idx])
         fileOut.close
-
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Cannot dissassemble this arguement.")
+        print("Cannot dissassemble this argument.")
     else:
         main(sys.argv[1])      
